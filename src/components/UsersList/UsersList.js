66f5af36d820086c9bfb319e35ed3a8fea7d1117
JeanCremonezzi <jeancremonezzi@hotmail.com>
2021-08-04 16:24:07 -0300

    Componente UsersList adicionado

---
+++ src/components/UsersList/UsersList.js
@@ -0,0 +1,20 @@
+import styles from "../UsersList/UsersList.module.css";
+import Card from "../Card/Card";
+
+function UsersList(props) {
+	return (
+		<Card className={styles.users}>
+			<ul>
+				{props.users.map((user) => {
+					return (
+						<li>
+							{user.username} ({user.age} years old)
+						</li>
+					);
+				})}
+			</ul>
+		</Card>
+	);
+}
+
+export default UsersList;
